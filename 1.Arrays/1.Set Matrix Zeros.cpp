dc9bace4ad7999e8982792e19e813ca54f000575
Ashish Dixit <ashishdixit815@gmail.com>
2023-06-19 00:03:11 +0530

    19-06-2023

---
+++ 1.Arrays/1.Set Matrix Zeros.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/*
+?Set Matrix Zero
+! Problem Statement: Given a matrix if an element in the matrix is 0 then you will have to set 
+! its entire column and row to 0 and then return the matrix.
+*/
+
+//! Bruteforce approach
+void setZeroes(vector<vector<int>>& matrix) {
+        
+}
+
+//! Better Approach
+void setZeroes(vector<vector<int>>& matrix) {
+        
+}
+
+//! Optimal Approach
+void setZeroes(vector<vector<int>>& matrix) {
+        
+}
