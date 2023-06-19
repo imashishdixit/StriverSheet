4ff9c22b6eeb239beab8fe42495b584d9424600e
Ashish Dixit <ashishdixit815@gmail.com>
2023-06-20 00:25:47 +0530

    set matrix zeros

--- 1.Arrays/1.Set Matrix Zeros.cpp
+++ 1.Arrays/1.Set Matrix Zeros.cpp
@@ -1,4 +1,6 @@
-#include<bits/stdc++.h>
+#include<bit>
+#include<vector.h>
+#include<bitstdc++.h>
 
 using namespace std;
 
@@ -9,16 +11,105 @@ using namespace std;
 */
 
 //! Bruteforce approach
+//? Time Complexity O((m*n)*(m+n))+O(m*n) Space Complexity O(1)
+void markRow(vector<vector<int>>& matrix,int i){
+    int n=matrix[0].size()̦;
+    for(int j=0;j<n;j++){
+        if(matrix[i][j]!=0){
+            matrix[i][j]=-1;
+        }
+    }
+    return;
+}
+void markCol(vector<vector<int>>& matrix,int j){
+     int m=matrix.size()̦;
+    for(int i=0;i<m;i++){
+        if(matrix[i][j]!=0){
+            matrix[i][j]=-1;
+        }
+    }
+    return;
+}
 void setZeroes(vector<vector<int>>& matrix) {
-        
+        int m=matrix.size();
+        int n=matrix[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    markRow(matrix,i); 
+                    markCol(matrix,j); 
+                }
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==-1){
+                    matrix[i][j]=0;
+                }
+            }
+        }
 }
 
 //! Better Approach
+//? Time Complexity O(2*m*n) Space Complexity O(m) + O(n)
 void setZeroes(vector<vector<int>>& matrix) {
-        
+        int m = matrix.size();
+        int n= matrix[0].size();
+        vector<int> row(m,0);
+        vector<int> col(n,0);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    row[i]=1;
+                    col[j]=1;
+                }
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(row[i] || col[j]){
+                    matrix[i][j]=0;
+                }
+            }
+        }
 }
 
 //! Optimal Approach
+//? Time Complexity O(m*n) Space Complexity O(1)
 void setZeroes(vector<vector<int>>& matrix) {
-        
+        int m = matrix.size();     
+        int n= matrix[0].size();
+        int col0 = 1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+
+                    if(j!=0) //? except first col
+                        matrix[0][j]=0;
+                    else  //! for the first col
+                        col0 = 0;
+                }
+            }
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if(matrix[0][0]==0){  //? if the first element 
+            for(int i=0;i<n;i++){
+                matrix[0][i]=0;
+            }
+        }
+        if(col0==0){
+            for(int i=0;i<m;i++){
+                matrix[i][0]=0;
+            }
+        }
+        return;
+
 }
+
