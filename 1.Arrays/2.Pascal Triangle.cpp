6d94a551f66dc8ad87bf3bf8b390aab1c666928c
Ashish Dixit <ashishdixit815@gmail.com>
2023-09-06 20:04:30 +0530

    changes

---
+++ 1.Arrays/2.Pascal Triangle.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+Variation 1: Given row number r and column number c. Print the element at position (r, c) in Pascal’s triangle.
+
+Variation 2: Given the row number n. Print the n-th row of Pascal’s triangle.
+
+Variation 3: Given the number of rows n. Print the first n rows of Pascal’s triangle.
+*/
+
+//? Variation 1 optimal solution
+
+int pascalElement (int r,int c){
+
+    return res;
+}
