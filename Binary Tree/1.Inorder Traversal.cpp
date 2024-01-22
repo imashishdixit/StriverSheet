ca06f92c7393eee29bab93f491c176812ae6b3b8
Ashish Dixit <ashishdixit815@gmail.com>
2024-01-22 21:47:00 +0530

    changes

---
+++ Binary Tree/1.Inorder Traversal.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<set>
+#include<map>
+#include<vector>
+#include<stack>
+using namespace std;
+/*
+? Problem Statement : Given a Binary Tree. Find and print the inorder traversal of Binary Tree.
+
+*/
+struct node {
+  int data;
+  struct node * left, * right;
+  node(int data){
+    this->data=data;
+    this->left = NULL;
+    this->right=NULL;
+  }
+};
+
+
+
+//! Iterative 
+//! Time Complexity: O(N).
+
+//! Reason: We are traversing N nodes and every node is visited exactly once.
+
+//! Space Complexity: O(N)
+
+//!Reason: In the worst case (a tree with just left children), the space complexity will be O(N).
+
+vector < int > inOrderTrav(node * curr) {
+  vector < int > inOrder;
+  stack < node * > s;
+  while (true) {
+    if (curr != NULL) {
+      s.push(curr);
+      curr = curr -> left;
+    } else {
+      if (s.empty()) break;
+      curr = s.top();
+      inOrder.push_back(curr -> data);
+      s.pop();
+      curr = curr -> right;
+    }
+  }
+  return inOrder;
+}
+
+//! Recursive
+
+
+
+int main(){
+
+}
